9274a4b72b8505d070119f9ac2e4a45e5c0cb729
celiltat2361 <celilbtc@gmail.com>
2021-12-03 14:49:12 +0100

    added codes

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import "./testimonials.scss"
+
+const Testimonials = () => {
+  return (
+    <div className="testimonials">
+      Testimonials
+    </div>
+  )
+}
+
+export default Testimonials
