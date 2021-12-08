18ad8f1a9049375bb97500c6885ef7d850f5fc1e
celiltat2361 <celilbtc@gmail.com>
2021-12-08 07:31:52 +0100

    added menu bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
-const Topbar = () => {
+const Topbar = ({menuOpen, setMenuOpen}) => {
     return (
-        <div className="topbar">
-           topbar 
+        <div className={"topbar " + (menuOpen && "active")}>
+           <div className="wrapper">
+            <div className="left">
+                <a href="#intro" className="logo">CllTt.</a>
+                <div className="itemContainer">
+                    <Person className="icon" />
+                    <span>+46 73 730 0279</span>
+                </div>
+                <div className="itemContainer">
+                    <Mail className="icon" />
+                    <span>23celiltat61@gmail.com</span>
+                </div>
+            </div>
+            
+            <div className="right">
+                <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                    <span className="line1"></span>
+                    <span className="line2"></span>
+                    <span className="line3"></span>
+                </div>
+            </div> 
+           </div>
         </div>
     )
 }
